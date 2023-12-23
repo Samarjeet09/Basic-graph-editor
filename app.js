061f383067f340c1d9d6b799c1923b215bc0a8c0
Samarjeet09 <singhsamarjeet09@gmail.com>
2023-12-23 16:24:40 +0530

    basic drawing completed

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 import { Graph } from "./js/math/graph.js";
 import { Point } from "./js/primitives/point.js";
-import { segment } from "./js/primitives/segment.js"; 
+import { Segment } from "./js/primitives/segment.js";
+
+
+
 document.addEventListener("DOMContentLoaded", function () {
   const can1 = document.getElementById("can1");
   can1.width = 600;
   can1.height = 600;
 
+  
   // get the context
   const ctx = can1.getContext("2d");
   const p1 = new Point(200, 200);
@@ -13,6 +17,11 @@ document.addEventListener("DOMContentLoaded", function () {
   const p3 = new Point(200, 400);
   const p4 = new Point(500, 200);
 
-  const graph = new Graph([p1, p2, p3, p4]);
+  const s1 = new Segment(p1, p2);
+  const s2 = new Segment(p1, p3);
+  const s3 = new Segment(p3, p2);
+  const s4 = new Segment(p4, p2);
+
+  const graph = new Graph([p1, p2, p3, p4], [s1, s2, s3, s4]);
   graph.draw(ctx);
 });
